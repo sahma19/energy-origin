531ddc1f5e545d2085545d06f61131a6e0489f08
Christoffer Frydkjær <christofferfk@gmail.com>
2025-04-10 08:40:58 +0200

    change validationproblemdetails

--- domains/authorization/Authorization.API/API/Authorization/Controllers/CredentialController.cs
+++ domains/authorization/Authorization.API/API/Authorization/Controllers/CredentialController.cs
@@ -25,9 +25,9 @@ public class CredentialController(
 {
     [HttpPost]
     [ProducesResponseType(typeof(CreateCredentialResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(CreateCredentialResponse), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(CreateCredentialResponse), StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(typeof(CreateCredentialResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),  StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),  StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),  StatusCodes.Status404NotFound)]
     [SwaggerOperation(
         Summary = "Create credential",
         Description = "Creates a single credential for a client"
@@ -50,9 +50,9 @@ public class CredentialController(
 
     [HttpGet]
     [ProducesResponseType(typeof(GetCredentialsResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(GetCredentialsResponse), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(GetCredentialsResponse), StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(typeof(GetCredentialsResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),  StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),  StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
     [SwaggerOperation(
         Summary = "Gets credentials",
         Description = "Gets all credentials for a client"
@@ -77,9 +77,9 @@ public class CredentialController(
     [HttpDelete]
     [Route("{keyId:guid}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
     [SwaggerOperation(
         Summary = "Deletes credential",
         Description = "Deletes a single credential for a client"
